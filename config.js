b06e2048eb5cd5c8d1ad10142b1a4ade71dc1ff6
GabiCMendes <gabrielacarvalhomendes2008@gmail.com>
2023-06-16 20:52:43 -0300

    projeto 71

--- config.js
+++ config.js
@@ -1,10 +1,15 @@
 import firebase from "firebase";
 require("@firebase/firestore");
 
-var firebaseConfig = {
-  //Cole sua Configuração do Firebase aqui
-  
+const firebaseConfig = {
+  apiKey: "AIzaSyAHk0gHBfTyJigThdUMOPpmqEA5swF5uNg",
+  authDomain: "ciclista-eletronico-4.firebaseapp.com",
+  projectId: "ciclista-eletronico-4",
+  storageBucket: "ciclista-eletronico-4.appspot.com",
+  messagingSenderId: "281353467356",
+  appId: "1:281353467356:web:86fb02e2012605ab9e8b19"
 };
+
 
 firebase.initializeApp(firebaseConfig);
 
